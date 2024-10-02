cf29b7f5f14b6e507f791e9d6b5c4dd34cd156b5
Esther Ogundijo <sheyiogundijo@gmail.com>
2024-10-02 10:17:00 +0100

    updates contact list  styles and adds pagination

--- src/components/contact/list.tsx
+++ src/components/contact/list.tsx
@@ -1,65 +1,94 @@
+import { useRouter } from "next/router";
+import Pagination from "../ui/pagination";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../ui/table";
-import { useGetContacts } from "src/hooks/contact";
+import { useGetPaginatedContacts } from "src/hooks/contact";
 
 export default function ContactList() {
-  const { contacts } = useGetContacts();
+  const router = useRouter();
+  const { data: contacts, meta_data } = useGetPaginatedContacts(
+    Number(router?.query?.page ?? 1)
+  );
+
+  const handlePageChange = (pageToGo: number) => {
+    router.push(`${router.pathname}?page=${pageToGo}`, undefined, {
+      shallow: true,
+    });
+  };
   return (
-    <div>
-      <h2 className="text-lg font-semibold mb-4">Contact List</h2>
-      <Table>
-        <TableCaption>A list of your recently added contacts.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Phone</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>Address</TableHead>
-            <TableHead>Longitude</TableHead>
-            <TableHead>Latitude</TableHead>
-          </TableRow>
-        </TableHeader>
-        {contacts && contacts.length ? (
-          <TableBody>
-            {contacts.map(
-              (contact: {
-                name: string;
-                phoneNumber: string;
-                email: string;
-                locationDetails: {
-                  address: string;
-                  longitude: string;
-                  latitude: string;
-                }[];
-              }) => (
-                <TableRow key={contact.name}>
-                  <TableCell className="font-medium">{contact.name}</TableCell>
-                  <TableCell>{contact.phoneNumber}</TableCell>
-                  <TableCell>{contact.email}</TableCell>
-                  <TableCell>{contact.locationDetails[0].address}</TableCell>
-                  <TableCell>{contact.locationDetails[0].longitude}</TableCell>
-                  <TableCell>{contact.locationDetails[0].latitude}</TableCell>
+    <Pagination
+      metaData={{
+        ...meta_data,
+      }}
+      handlePageChange={handlePageChange}
+    >
+      <div>
+        <h2 className="text-lg font-semibold mb-4">Contact List</h2>
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Phone</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Address</TableHead>
+                <TableHead>Longitude</TableHead>
+                <TableHead>Latitude</TableHead>
+              </TableRow>
+            </TableHeader>
+            {contacts && contacts.length ? (
+              <TableBody>
+                {contacts.map(
+                  (contact: {
+                    name: string;
+                    phoneNumber: string;
+                    email: string;
+                    locationDetails: {
+                      address: string;
+                      longitude: string;
+                      latitude: string;
+                    }[];
+                  }) => (
+                    <TableRow key={contact.name}>
+                      <TableCell className="font-medium">
+                        {contact.name}
+                      </TableCell>
+                      <TableCell>{contact.phoneNumber}</TableCell>
+                      <TableCell>{contact.email}</TableCell>
+                      <TableCell
+                        title={contact.locationDetails[0].address || ""}
+                        className="whitespace-nowrap max-w-[300px] truncate"
+                      >
+                        {contact.locationDetails[0].address}
+                      </TableCell>
+                      <TableCell>
+                        {contact.locationDetails[0].longitude}
+                      </TableCell>
+                      <TableCell>
+                        {contact.locationDetails[0].latitude}
+                      </TableCell>
+                    </TableRow>
+                  )
+                )}
+              </TableBody>
+            ) : (
+              <TableBody>
+                <TableRow>
+                  <TableCell colSpan={6} className="text-center">
+                    No contacts found
+                  </TableCell>
                 </TableRow>
-              )
+              </TableBody>
             )}
-          </TableBody>
-        ) : (
-          <TableBody>
-            <TableRow>
-              <TableCell colSpan={6} className="text-center">
-                No contacts found
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        )}
-      </Table>
-    </div>
+          </Table>
+        </div>
+      </div>
+    </Pagination>
   );
 }
