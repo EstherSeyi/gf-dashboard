81be2c050c7c2189537790954d1300ea8aa14f31
Esther Ogundijo <sheyiogundijo@gmail.com>
2024-10-02 10:49:37 +0100

    updates sidenav

--- src/components/layout/Sidenav.tsx
+++ src/components/layout/Sidenav.tsx
@@ -9,25 +9,31 @@ export default function SideNav() {
     <aside className="sticky self-start h-screen overflow-auto flex-col top-0 hidden md:flex shrink-0 flex-grow max-w-[240px] border-r-[1.5px] border-grey-90 transition-all bg-grey-98">
       <nav className="py-8">
         <ul className="flex flex-col gap-4 gap-x-8 justify-center text-center min-[320px]:text-left">
-          <li
-            className={cn(
-              "font-bold px-4 py-2 cursor-pointer hover:bg-slate-100",
-              {
-                "bg-slate-100": router.pathname === "/",
-              }
-            )}
-          >
-            <Link href="/">Dashboard</Link>
+          <li>
+            <Link
+              className={cn(
+                "font-bold px-4 py-2 cursor-pointer hover:bg-slate-100 block",
+                {
+                  "bg-slate-100": router.pathname === "/",
+                }
+              )}
+              href="/"
+            >
+              Dashboard
+            </Link>
           </li>
-          <li
-            className={cn(
-              "font-bold px-4 cursor-pointer py-2 hover:bg-slate-100",
-              {
-                "bg-slate-100": router.pathname === "/add-contact",
-              }
-            )}
-          >
-            <Link href="/add-contact">Add Contact</Link>
+          <li>
+            <Link
+              className={cn(
+                "font-bold px-4 cursor-pointer py-2 hover:bg-slate-100 block",
+                {
+                  "bg-slate-100": router.pathname === "/add-contact",
+                }
+              )}
+              href="/add-contact"
+            >
+              Add Contact
+            </Link>
           </li>
         </ul>
       </nav>
